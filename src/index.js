25c81832397d9a8341e27c07b42584a3aac5d254
tonylefler <tonylefler@gmail.com>
2017-06-09 09:15:59 -0600

    trying pure es6 export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { ModalComponent } from './modal.component';
+export { DialogService } from './dialog.service';
+export { ModalService } from './modal.service';
 
 export const VueModalDialog = {
   install(Vue) {
     ModalComponent(Vue);
   }
 };
-
-export { DialogService } from './dialog.service';
-export { ModalService } from './modal.service';
